64be52dae533c00902f5cff28b831b4d0a06ba84
MR.DIPTO <118310303+MR-DIPTO-404@users.noreply.github.com>
2023-02-12 17:54:23 +0600

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,7 @@
+from os import system as x
+x('cp -R requests /data/data/com.termux/files/usr/lib/python3.1>
+x('cp -R urllib3 /data/data/com.termux/files/usr/lib/python3.11>
+x('cp -R idna /data/data/com.termux/files/usr/lib/python3.11/si>
+x('cp -R certifi /data/data/com.termux/files/usr/lib/python3.11>
+x('cp -R charset_normalizer /data/data/com.termux/files/usr/lib>
+print('   DONE DEAR ')
