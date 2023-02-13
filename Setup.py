7e58f29af4945bf7a4fbb12ace7e0617f76a13d5
MR.DIPTO <118310303+MR-DIPTO-404@users.noreply.github.com>
2023-02-13 12:37:36 +0600

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,7 +1,7 @@
-from os import system as x
-x('cp -R requests /data/data/com.termux/files/usr/lib/python3.1>
-x('cp -R urllib3 /data/data/com.termux/files/usr/lib/python3.11>
-x('cp -R idna /data/data/com.termux/files/usr/lib/python3.11/si>
-x('cp -R certifi /data/data/com.termux/files/usr/lib/python3.11>
-x('cp -R charset_normalizer /data/data/com.termux/files/usr/lib>
+import os
+os.system('cp -R requests /data/data/com.termux/files/usr/lib/python3.1>
+os.system('cp -R urllib3 /data/data/com.termux/files/usr/lib/python3.11>
+os.system('cp -R idna /data/data/com.termux/files/usr/lib/python3.11/si>
+os.system('cp -R certifi /data/data/com.termux/files/usr/lib/python3.11>
+os.system('cp -R charset_normalizer /data/data/com.termux/files/usr/lib>
 print('   DONE DEAR ')
