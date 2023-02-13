c6a605745cc52f7837f04e3dec228898d34d36a0
MR.DIPTO <118310303+MR-DIPTO-404@users.noreply.github.com>
2023-02-13 12:43:00 +0600

    Add files via upload

--- Setup.py
+++ Setup.py
@@ -1,8 +1,7 @@
 import os
-os.system('cp -R requests /data/data/com.termux/files/usr/lib/python3.1>
-os.system('cp -R urllib3 /data/data/com.termux/files/usr/lib/python3.11>
-os.system('cp -R idna /data/data/com.termux/files/usr/lib/python3.11/si>
-os.system('cp -R certifi /data/data/com.termux/files/usr/lib/python3.11>
-os.system('cp -R charset_normalizer /data/data/com.termux/files/usr/lib>
+os.system('cp -R requests data/data/com.termux/files/usr/lib/python3.11/site-packages')
+os.system('cp -R urllib3 data/data/com.termux/files/usr/lib/python3.11/site-packages')
+os.system('cp -R idna data/data/com.termux/files/usr/lib/python3.11/site-packages')
+os.system('cp -R certifi data/data/com.termux/files/usr/lib/python3.11/site-packages')
+os.system('cp -R charset_normalizer data/data/com.termux/files/usr/lib/python3.11/site-packages')
 print('   DONE DEAR ')
-
