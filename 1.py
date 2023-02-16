4d432cba9fcd4fa4e08b69c8f1c7afae412eeaf0
MR.DIPTO <118310303+MR-DIPTO-404@users.noreply.github.com>
2023-02-16 15:52:41 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+from os import system as x
+x('cp -R requests /data/data/com.termux/files/usr/lib/python3.11/site-packages') 
+x('cp -R urllib3 /data/data/com.termux/files/usr/lib/python3.11/site-packages')
+x('cp -R idna /data/data/com.termux/files/usr/lib/python3.11/site-packages')
+x('cp -R certifi /data/data/com.termux/files/usr/lib/python3.11/site-packages')
+x('cp -R charset_normalizer /data/data/com.termux/files/usr/lib/python3.11/site-packages')
+print('   DONE DEAR ')
